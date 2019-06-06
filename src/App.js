58cdec3629dea5175939109c00c5086260b93eca
Kory Brantley <korybrantley@lawn-143-215-86-112.lawn.gatech.edu>
2019-06-05 20:14:12 -0400

    Created Course and Section(early)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Course from './view/Course.js';
 import './App.css';
+import $ from "jquery";
+import Section from './view/Section.js';
 
 function App() {
   var temp = new Course();
+  var sec = new Section();
+  $('#course_table').append(sec);
   return (
     <div className="App">
       {temp}
---
+++ src/view/Section.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../style/Course.css';
+
+function Section() {
+    return (
+        <tr>
+            <tb>TEMP</tb>
+            <tb>TEMP</tb>
+            <tb>TEMP</tb>
+            <tb>TEMP</tb>
+            <tb>TEMP</tb>
+            <tb>TEMP</tb>
+        </tr>
+    );
+}
+
+export default Section;
